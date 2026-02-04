31976932df348d2948afcc025c966d613cba5092
thenerdworthyone <thenerdworthyone@gmail.com>
2026-02-04 21:37:22 +1100

    blerh
    bleh

--- weather.js
+++ weather.js
@@ -5,18 +5,27 @@ dotenv.config();
 const weatherForm = document.querySelector(".weatherapi");
 const Cityinput = document.querySelector(".cityinput");
 const Card = document.querySelector(".card");
-const apiKey = process.env.API_KEY;
+const apiKey= process.env.API_KEY;
+console.log(apiKey);
 
 
 
-
-weatherForm.addEventListener("submit", event=> {
+weatherForm.addEventListener("submit", async event=> {
 
   event.preventDefault();
 
   const city = Cityinput.value;
 
-  if (city){
+ if (city){
+     try{
+        const weatherData= await getWeatherData(city);
+        displayWeatherInfo(weatherData);
+
+     }
+    catch (error){
+        console.error(error);
+        displayError(error);
+    }
 
   }
   else{
@@ -45,7 +54,7 @@ function displayError(message){
     errorDisplay.textContent = message;
     errorDisplay.classList.add("errordisplay");
 
-    Card.textContext ="";
+    Card.textContent ="";
     Card.style.display = 'flex';
     Card.appendChild(errorDisplay);
 
